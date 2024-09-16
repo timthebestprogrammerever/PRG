5865b46324ef4346f6bda0a73a76114b352bad73
Vitalij Atamanjuk <atamanjuk@infis.cz>
2024-09-16 09:32:55 +0200

    Example of variables used in Java

---
+++ 1_Září/Variables.java
@@ -0,0 +1,32 @@
+public class Variables {
+    public static void main(String[] args) {
+
+        // type variableName = value;
+
+        // numeric values
+        {
+            byte a = 1;         // -128 - 127
+            short b = 1;        // -32 768 - 32 767
+            int c = 1;          // 2^31 - 2^31 - 1
+            long d = 1L;        // 2^63 - 2^63 - 1
+        }
+
+        // numeric values with a decimal point
+        {
+            float e = 1.0f;
+            double f = 1.0d;
+        }
+
+        // true/false values
+        {
+            boolean g = true;
+            boolean h = false;
+        }
+
+        // text and letters
+        {
+            char i = 'a';
+            String j = "abc";
+        }
+    }
+}
